546d3f4219e3c7013f7135160127ff5de125a536
MaGus <magus.go27@gmail.com>
2023-05-21 20:53:49 -0300

    SQL formatting

--- dags/dag_TP_AWS.py
+++ dags/dag_TP_AWS.py
@@ -100,7 +100,7 @@ def ProcessCSV():
         
         # Insert data into the table
         for index, row in df.iterrows():
-            insert_query = f"INSERT INTO TopProduct (advertiser_id, product_id, topProduct) VALUES ({row['advertiser_id']}, {row['product_id']}, {row['count']})"
+            insert_query = f"INSERT INTO topproduct (advertiser_id, product_id, top_product) VALUES (\'{row['advertiser_id']}\', \'{row['product_id']}\', {row['count']})"
             cursor.execute(insert_query)
             connection.commit()
         
